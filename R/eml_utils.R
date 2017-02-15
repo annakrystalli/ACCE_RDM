f29dad57848b452995ce8d40c97eb02f3ff64879
annakrystalli <annakrystalli@googlemail.com>
2017-02-15 12:27:21 +0000

    commit eml_utils

---
+++ R/eml_utils.R
@@ -0,0 +1,23 @@
+# ---- create_metadf ----
+create_metadf <- function(df){
+    rows <- ncol(df)
+    meta <- data.frame(attributeName = rep(NA, times = rows),
+                       attributeDefinition = rep(NA, times = rows),
+                       columnClasses = rep(NA, times = rows),
+                       measurementScale = rep(NA, times = rows),
+                       numberType = rep(NA, times = rows),
+                       unit = rep(NA, times = rows),
+                       minimum = rep(NA, times = rows),
+                       maximum = rep(NA, times = rows),
+                       formatString = rep(NA, times = rows),
+                       definition = rep(NA, times = rows), 
+                       code = rep(NA, times = rows),
+                       levels = rep(NA, times = rows),
+                       stringsAsFactors = FALSE)
+    
+    meta$attributeName <- names(df)
+    meta$columnClasses <- sapply(df, class)
+    meta$columnClasses[meta$columnClasses == "integer"] <- "numeric"
+    
+    return(meta)
+}
